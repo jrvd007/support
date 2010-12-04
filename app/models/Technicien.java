4b0ae497692321506012cfdaab8f5d91e63c575b
Matthew Leon Grinshpun <vertespain@gmail.com>
2010-12-03 23:03:20 -0500

    tech class added - no tests yet

---
+++ app/models/Technicien.java
@@ -0,0 +1,12 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+public class Technicien extends Usager {
+    public Technicien(String nom, String prenom, String telephone,
+            String courriel, String bureau, String username,
+            String password) {
+        super(nom, prenom, telephone, courriel, bureau, username, password);
+    }
+}

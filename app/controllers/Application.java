9ba85ed04a3234c2a26120098695d044bc7253c3
Matthew Leon Grinshpun <vertespain@gmail.com>
2010-12-07 15:31:03 -0500

    authentication with methods to check user access level

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,6 +10,16 @@ import models.*;
 @With(Secure.class)
 public class Application extends Controller {
 
+    @Before
+    static void setConnectedUser() {
+        if(Security.isConnected()) {
+            Usager user = Usager.find("byUsername", Security.connected())
+                                .first();
+            renderArgs.put("user", user);
+            renderArgs.put("isTech", Technicien.class.isInstance(user));
+        }
+    }
+
     public static void index() {
         List requetes = Requete.findAll();
         render(requetes);
